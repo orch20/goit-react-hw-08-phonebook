bde915146e2da4ee7373c2c50a61cbd89793edaf
orch20 <94862153+orch20@users.noreply.github.com>
2022-09-04 22:58:25 +0300

    initial files

---
+++ src/redux/store.js
@@ -0,0 +1,32 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialStore = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+];
+
+const contactsSlice = createSlice({
+  name: 'contacts',
+  initialState: initialStore,
+  reducers: {
+    add(state, action) {
+      state.push(action.payload);
+    },
+    remove(state, action) {
+      return state.filter(({ id }) => id !== action.payload);
+    },
+  },
+});
+
+export const { add, remove } = contactsSlice.actions;
+
+contactsSlice(console.log('add, remove', add, remove));
+
+export const store = configureStore({
+  reducer: {
+    contacts: contactsSlice.reducer,
+  },
+});
