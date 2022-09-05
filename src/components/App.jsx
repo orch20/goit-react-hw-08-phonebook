27d2b0107c3858f98875b04ce61d2d17aec03690
orch20 <94862153+orch20@users.noreply.github.com>
2022-09-05 22:13:13 +0300

    refactored app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,53 +1,60 @@
-import React, { useState, useEffect } from 'react';
+// useEffect(() => {
+//   localStorage.setItem('contacts', JSON.stringify(contacts));
+// const contactsExist = localStorage.getItem('contacts');
+// if (contactsExist) {
+//   setContacts(JSON.parse(contactsExist));
+// }
+// }, [contacts]);
+
+import React, { useEffect } from 'react';
 import { FormContacts } from './FormContacts/FormContacts';
 import { RenderContactsList } from './RenderContactsList/RenderContactsList';
 import { Filter } from './Filter/Filter';
 import { Container } from './Container.styled';
-import { nanoid } from 'nanoid';
 
-const initialValues = [
-  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-];
+import { useSelector, useDispatch } from 'react-redux';
+import { addContacts, removeContacts } from '../redux/contacts/contacts-slice';
+
+import { setFilter } from 'redux/filter/filter-slice';
+
+import { getContacts } from 'redux/contacts/contacts-selectors';
+import { getFilter } from 'redux/filter/filter-selectors';
 
 export const App = () => {
-  const [contacts, setContacts] = useState([...initialValues]);
-  const [filter, setFilter] = useState('');
+  const contacts = useSelector(getContacts);
+  const filter = useSelector(getFilter);
 
   useEffect(() => {
     localStorage.setItem('contacts', JSON.stringify(contacts));
-    // const contactsExist = localStorage.getItem('contacts');
-    // if (contactsExist) {
-    //   setContacts(JSON.parse(contactsExist));
-    // }
   }, [contacts]);
 
-  const filterChange = event => {
-    setFilter(event.target.value);
-  };
+  // const getFilteredContacts = () =>
+  //   contacts.filter(contact =>
+  //     contact.name.toLowerCase().includes(filter.toLowerCase())
+  //   );
 
-  const getFilteredContacts = () =>
-    contacts.filter(contact =>
-      contact.name.toLowerCase().includes(filter.toLowerCase())
+  const dispatch = useDispatch();
+
+  const formSubmitHandler = payload => {
+    const findSimilarContact = contacts.find(
+      contact => contact.name === payload.name
     );
-
-  const deleteContact = id => {
-    setContacts(prevContact =>
-      prevContact.filter(contact => contact.id !== id)
-    );
-  };
-
-  const formSubmitHandler = ({ name, number }) => {
-    const newContact = { id: nanoid(5), name, number };
-    const findSimilarContact = contacts.find(contact => contact.name === name);
 
     if (findSimilarContact) {
       alert('Contact already exists');
     } else {
-      setContacts([...contacts, newContact]);
+      const action = addContacts(payload);
+      dispatch(action);
     }
+  };
+  const deleteContact = payload => {
+    const action = removeContacts(payload);
+    dispatch(action);
+  };
+
+  const filterChange = event => {
+    const action = setFilter(event.target.value);
+    dispatch(action);
   };
 
   return (
@@ -58,89 +65,10 @@ export const App = () => {
       <Filter value={filter} onFilterChange={filterChange} />
       <>
         <RenderContactsList
-          contactsList={getFilteredContacts()}
+          // contactsList={getFilteredContacts()}
           onDeleteContact={deleteContact}
         />
       </>
     </Container>
   );
 };
-
-// export class App extends Component {
-//   state = {
-//     contacts: [...initialValues],
-//     filter: '',
-//   };
-
-// ------------------------------------------------
-
-// componentDidMount() {
-//   const contacts = localStorage.getItem('contacts');
-//   if (contacts) {
-//     this.setState({ contacts: JSON.parse(contacts) });
-//   }
-// }
-
-// componentDidUpdate(prevProps, prevState) {
-//   if (prevState.contacts !== this.state.contacts) {
-//     localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-//     console.log('didUpdate :>> ', 'didUpdate');
-//   }
-// }
-// ------------------------------------------------------
-
-// filterChange = event => {
-//   this.setState({ filter: event.target.value });
-// };
-
-// getFilteredContacts = () => {
-//   const { contacts, filter } = this.state;
-//   return contacts.filter(contact =>
-//     contact.name.toLowerCase().includes(filter.toLowerCase())
-//   );
-// };
-
-// deleteContact = id => {
-//   this.setState(prevState => ({
-//     contacts: prevState.contacts.filter(contact => contact.id !== id),
-//   }));
-// };
-// ------------------------------------------------
-//   formSubmitHandler = data => {
-//     const { contacts } = this.state;
-//     data.id = nanoid(5);
-
-//     const findingSimilarContact = contacts.find(
-//       contact => contact.name === data.name
-//     );
-
-//     if (findingSimilarContact) {
-//       alert('Contact already exists');
-//     } else {
-//       this.setState(preState => {
-//         return {
-//           contacts: [...preState.contacts, data],
-//         };
-//       });
-//     }
-//   };
-
-//   render() {
-//     const { filter } = this.state;
-//     const filteredContacts = this.getFilteredContacts();
-//     return (
-//       <Container>
-//         <h1>Phonebook</h1>
-//         <FormContacts onSubmitProp={this.formSubmitHandler} />
-//         <h2>Contacts</h2>
-//         <Filter value={filter} onFilterChange={this.filterChange} />
-//         <>
-//           <RenderContactsList
-//             contactsList={filteredContacts}
-//             onDeleteContact={this.deleteContact}
-//           />
-//         </>
-//       </Container>
-//     );
-//   }
-// }
