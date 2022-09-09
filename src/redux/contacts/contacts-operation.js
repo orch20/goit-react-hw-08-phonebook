a860da4f447e93588aa92c8dd0493f0db77c087e
orch20 <orest.chorny@gmail.com>
2022-09-09 16:04:26 +0300

    added fetch

---
+++ src/redux/contacts/contacts-operation.js
@@ -0,0 +1,6 @@
+import { getContactsApi } from '../../shared/api';
+
+export const fetchContacts = () => {
+  const func = dispatch => {};
+  func();
+};
--- src/shared/api/contactsApi.js
+++ src/shared/api/contactsApi.js
@@ -1,5 +1,10 @@
 import axios from 'axios';
 
-const instance = axios.create(
-  'https://631a5378dc236c0b1edc313d.mockapi.io/contacts'
-);
+const instance = axios.create({
+  baseURL: 'https://631a5378dc236c0b1edc313d.mockapi.io/contacts',
+});
+
+const getContactsApi = async () => {
+  const { data } = await instance.get('/');
+  return data;
+};
