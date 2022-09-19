eee4f94c18ea2469a9bd6e904d776ca2a19ca87b
orch20 <94862153+orch20@users.noreply.github.com>
2022-09-19 21:25:33 +0300

    added redux-persist for token

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,17 +2,47 @@ import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import contactsSlice from './contacts/contacts-slice';
 import filterSlice from './filter/filter-slice';
 import authSlice from './auth/auth-slice';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 const Reducer = combineReducers({
   items: contactsSlice,
   filter: filterSlice,
 });
 
-const rootReducer = combineReducers({
-  contacts: Reducer,
-  auth: authSlice,
-});
+// const rootReducer = combineReducers({
+// contacts: Reducer,
+// auth: authSlice,
+// });
+
+const persistConfig = {
+  key: 'token',
+  storage,
+  whitelist: ['token'],
+};
+
+const persistedReducer = persistReducer(persistConfig, authSlice);
 
 export const store = configureStore({
-  reducer: rootReducer,
+  reducer: {
+    contacts: Reducer,
+    auth: persistedReducer,
+  },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
