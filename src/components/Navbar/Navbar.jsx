4d3fedb332fdc4dbef39fef322d7a2d27bef48cc
orch20 <94862153+orch20@users.noreply.github.com>
2022-09-22 23:35:09 +0300

    deleted bag

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,3 @@
-import { Link } from 'react-router-dom';
-
 import NavbarMenu from './NavbarMenu/NavbarMenu';
 import NavbarAuth from './NavbarAuth/NavbarAuth';
 import NavbarUser from './NavbarUser/NavbarUser';
